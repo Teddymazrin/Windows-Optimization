0d9922cae257326a2853ca2b7b747f4e963a43b3
Teddy Mazrin <79080359+Teddymazrin@users.noreply.github.com>
2025-01-04 23:12:25 -0600

    Update Discord Black.bat

--- Discord Black.bat
+++ Discord Black.bat
@@ -3,3 +3,4 @@ curl -o "%temp%\index.js" "https://raw.githubusercontent.com/Teddymazrin/Windows
 move "%temp%\index.js" "%localappdata%\Discord\app-1.0.9175\modules\discord_desktop_core-1\discord_desktop_core\"
 
 
+::Need to update app version for it to work. For example 'app-1.0.9175' if this updates. You need to change to the correct version which can be found in the file path.
