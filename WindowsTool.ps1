cf0ab282a9a166935e6e68612660070d1da567ea
Teddy Mazrin <79080359+Teddymazrin@users.noreply.github.com>
2025-07-19 22:49:57 -0500

    Update WindowsTool.ps1

--- WindowsTool.ps1
+++ WindowsTool.ps1
@@ -106,8 +106,10 @@ Clear-Host
         }
         "2" {
             Write-Host "Uninstalling Driver" -ForegroundColor Cyan
+            # Downloading Cleanup Tool
             Invoke-WebRequest -Uri "https://github.com/Teddymazrin/Windows-Optimization/raw/refs/heads/main/Programs/CleanupTool.exe" -OutFile "$env:TEMP/CleanupTool.exe"
-	    Start-Process -FilePath "$env:TEMP/CleanupTool.exe"
+	    # Starting Cleanup Tool
+            Start-Process -FilePath "$env:TEMP/CleanupTool.exe"
         }
 	
         "3" {
